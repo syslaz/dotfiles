ada181691ee599b730b7b3a61ac3da2f5202a5d6
root <root@workstation>
2024-05-28 04:16:12 -0500

    fixed copy/paste

--- home/config/nvim/lua/config/options.lua
+++ home/config/nvim/lua/config/options.lua
@@ -5,3 +5,17 @@
 vim.api.nvim_command("autocmd BufRead,BufNewFile *.wsb set filetype=xml")
 
 vim.g.minipairs_disable = true -- disable auto quotes etc
+
+if vim.env.SSH_TTY or vim.env.SSH_CONNECTION then
+  vim.g.clipboard = {
+    name = "OSC 52",
+    copy = {
+      ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+    },
+    -- paste = {
+    -- ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+    -- ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+    -- },
+  }
+end
