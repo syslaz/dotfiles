f5fd8d485f02b0e7a6cda6f1aa9781394a2469f4
root <root@workstation>
2024-05-28 03:21:20 -0500

    fixed ssh copy with OSC 52.

--- home/config/nvim/lua/config/options.lua
+++ home/config/nvim/lua/config/options.lua
@@ -4,43 +4,22 @@
 
 vim.api.nvim_command("autocmd BufRead,BufNewFile *.wsb set filetype=xml")
 
--- vim.g.autoformat = false
-
 vim.g.minipairs_disable = true -- disable auto quotes etc
--- vim.opt.wrap = true
---
---#region
--- vim.opt.shellslash = false
 
--- vim.opt.shell = vim.fn.executable('pwsh') and 'pwsh' or 'powershell'
--- vim.opt.shellcmdflag =
--- "-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues['Out-File:Encoding']='utf8';Remove-Alias -Force -ErrorAction SilentlyContinue tee;"
--- vim.opt.shellredir = '2>&1 | %%{ "$_" } | Out-File %s; exit $LastExitCode'
--- vim.opt.shellpipe = '2>&1 | %%{ "$_" } | tee %s; exit $LastExitCode'
--- vim.opt.shellquote = ""
--- vim.opt.shellxquote = ""
--- vim.opt.shellslash = false
+function no_paste(reg)
+  return function(lines) end
+end
 
--- vim.lsp.buf.timeout_ms = 5000
-
--- vim.opt.shell
-
--- vim.opt.shell = vim.fn.executable "pwsh" and "pwsh" or "powershell"
--- vim.opt.shellcmdflag =
--- "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
--- vim.opt.shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait"
--- vim.opt.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
--- vim.opt.shellquote = "\""
--- vim.opt.shellxquote = ""
-
--- vim.g.clipboard = {
---   name = 'OSC 52',
---   copy = {
---     ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
---     ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
---   },
--- paste = {
---   ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
---   ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
--- },
--- }
+if os.getenv("SSH_TTY") then
+  vim.g.clipboard = {
+    name = "OSC 52",
+    copy = {
+      ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+    },
+    paste = {
+      ["+"] = no_paste("+"), -- pasting disabled
+      ["*"] = no_paste("*"), -- pasting disabled
+    },
+  }
+end
